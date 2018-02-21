2784324e5484a357fda3e136215477b8b7fd161d
dev-angelica-payawal <dev.angelica.payawal@gmail.com>
2018-02-21 11:57:21 +0800

    add theme

--- PVMTrading_v1/App_Start/BundleConfig.cs
+++ PVMTrading_v1/App_Start/BundleConfig.cs
@@ -26,7 +26,7 @@ namespace PVMTrading_v1
                      
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css",
                       "~/Content/datatables/css/datatables.bootstrap.css"));
 
