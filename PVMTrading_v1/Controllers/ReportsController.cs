19cc7faf06b73255e36a2569e96fea186d44a425
dev-angelica-payawal <dev.angelica.payawal@gmail.com>
2018-02-25 07:28:21 +0800

    database

--- PVMTrading_v1/Controllers/ReportsController.cs
+++ PVMTrading_v1/Controllers/ReportsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -32,10 +33,10 @@ namespace PVMTrading_v1.Controllers
         }
 
         // GET: ExportData
-        public ActionResult ExportToExcel()
+        public ActionResult CashTransactionReport()
         {
             // Step 1 - get the data from database
-            var data = _context.CashTransactions.ToList();
+            var data = _context.CashTransactions.Include(c => c.Customer).ToList();
 
             // instantiate the GridView control from System.Web.UI.WebControls namespace
             // set the data source
@@ -47,7 +48,7 @@ namespace PVMTrading_v1.Controllers
             Response.ClearContent();
             Response.Buffer = true;
             // set the header
-            Response.AddHeader("content-disposition", "attachment;filename = itfunda.xls");
+            Response.AddHeader("content-disposition", "attachment;filename = CashTransaction.xls");
             Response.ContentType = "application/ms-excel";
             Response.Charset = "";
             // create HtmlTextWriter object with StringWriter
@@ -66,7 +67,73 @@ namespace PVMTrading_v1.Controllers
             return View();
         }
 
+        public ActionResult LayAwayTransactionReport()
+        {
+            // Step 1 - get the data from database
+            var data = _context.LayAwayTransactions.Include(c => c.Product)
+                                                   .Include(c => c.Customer).ToList();
 
+            // instantiate the GridView control from System.Web.UI.WebControls namespace
+            // set the data source
+            GridView gridview = new GridView();
+            gridview.DataSource = data;
+            gridview.DataBind();
 
+            // Clear all the content from the current response
+            Response.ClearContent();
+            Response.Buffer = true;
+            // set the header
+            Response.AddHeader("content-disposition", "attachment;filename = LayAway.xls");
+            Response.ContentType = "application/ms-excel";
+            Response.Charset = "";
+            // create HtmlTextWriter object with StringWriter
+            using (StringWriter sw = new StringWriter())
+            {
+                using (HtmlTextWriter htw = new HtmlTextWriter(sw))
+                {
+                    // render the GridView to the HtmlTextWriter
+                    gridview.RenderControl(htw);
+                    // Output the GridView content saved into StringWriter
+                    Response.Output.Write(sw.ToString());
+                    Response.Flush();
+                    Response.End();
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Loan()
+        {
+            // Step 1 - get the data from database
+            var data = _context.CashTransactions.Include(c => c.Customer).ToList();
+
+            // instantiate the GridView control from System.Web.UI.WebControls namespace
+            // set the data source
+            GridView gridview = new GridView();
+            gridview.DataSource = data;
+            gridview.DataBind();
+
+            // Clear all the content from the current response
+            Response.ClearContent();
+            Response.Buffer = true;
+            // set the header
+            Response.AddHeader("content-disposition", "attachment;filename = LayAway.xls");
+            Response.ContentType = "application/ms-excel";
+            Response.Charset = "";
+            // create HtmlTextWriter object with StringWriter
+            using (StringWriter sw = new StringWriter())
+            {
+                using (HtmlTextWriter htw = new HtmlTextWriter(sw))
+                {
+                    // render the GridView to the HtmlTextWriter
+                    gridview.RenderControl(htw);
+                    // Output the GridView content saved into StringWriter
+                    Response.Output.Write(sw.ToString());
+                    Response.Flush();
+                    Response.End();
+                }
+            }
+            return View();
+        }
     }
 }
